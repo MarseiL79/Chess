6fd406156457e66b7d9edab1f7e32c35d0eebf4e
Max <cotliarow.maxim@yandex.ru>
2025-03-14 21:29:12 +0700

    Pieces can move now, added highlighting of possible moves, added ability to "eat" enemy pieces, added turn-based logic (black/white turn), added rectangle which shows is it black or white turn now.

---
+++ src/main/java/ru/apache_maven/GameLogic.java
@@ -0,0 +1,30 @@
+package ru.apache_maven;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class GameLogic {
+    ColorChess turnColor;
+    public GameLogic() {
+        turnColor = ColorChess.WHITE;
+    }
+
+    public ColorChess getTurnColor() {
+        return turnColor;
+    }
+    public void turn() {
+
+    }
+
+    public void changeTurnColor(Rectangle turnColorRectangle) {
+        if (turnColor == ColorChess.WHITE) {
+            turnColor = ColorChess.BLACK; System.out.println("Ход черных");
+            turnColorRectangle.setFill(Color.BLACK);
+        }
+        else {
+            turnColor = ColorChess.WHITE; System.out.println("Ход белых");
+            turnColorRectangle.setFill(Color.WHITE);
+        }
+    }
+}
---
+++ src/main/java/ru/apache_maven/Player.java
@@ -0,0 +1,7 @@
+package ru.apache_maven;
+
+public class Player {
+    ColorChess colorPlayer;
+
+    public ColorChess getColorPlayer() { return colorPlayer; }
+}
